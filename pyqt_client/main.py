389a2f86d7f60cda97375725612f0cd696861736
Pentabyteman <nils@brennbach.de>
2017-10-14 21:52:55 +0200

    Update main.py

--- pyqt_client/main.py
+++ pyqt_client/main.py
@@ -1,80 +1,83 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-import sys
-import json
-import socket_client
-import itertools
-
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtGui, uic
-
-class VotingClient(QMainWindow, socket_client.SocketClient):
-
-    data_received = pyqtSignal(str)
+import socket
+import struct
+import ssl
+import pickle
+from threading import Thread
+import select
+from PyQt5.QtCore import QObject
 
 
-    def __init__(self, *args, **kwargs):
-        super(VotingClient, self).__init__(*args, **kwargs)
-        self.data_received.connect(self.on_receive)
-        uic.loadUi('muscom.ui', self)
-        self.lastvote = ""
-        self.show()
-        self.connect("10.42.24.206", 54345)
-        self.buttonArray = [self.pushButton, self.pushButton_2, self.pushButton_3, self.pushButton_4,
-                            self.pushButton_5, self.pushButton_6, self.pushButton_7, self.pushButton_8,
-                            self.pushButton_9, self.pushButton_10, self.pushButton_11, self.pushButton_12,
-                            self.pushButton_13, self.pushButton_14, self.pushButton_15, self.pushButton_16,
-                            self.pushButton_17, self.pushButton_18, self.pushButton_19, self.pushButton_20,
-                            self.pushButton_21, self.pushButton_22, self.pushButton_23,
-                            self.pushButton_24, self.pushButton_25]
+class SocketClient(QObject):
+    def __init__(self):
+        super(SocketClient, self).__init__()
+        self.terminated = False
+        self.connected = False
+        self.log = " "
+        print("Initialized socket client")
+
+    def connect(self, host, port):
+        print("print")
+        try:
+            print("Connecting to", host)
+            self.socket = socket.socket()
+            self.socket.connect((host, port))
+            self.start()
+            self.connected = True
+            return True
+        except socket.error:
+            print("ERROR: Error while connecting!")
+            self.connected = False
+            return False
+
+    def start(self):
+        print("Client has started")
+        Thread(target=self.handle_server).start()
+
+    def disconnect(self):
+        print("disconnecting")
+        self.connected = False
+        self.send("quit")
+        self.terminated = True
+        self.socket.close()
+
+    def send(self, data):
+        try:
+            self.socket.sendall(data.encode("utf-8"))
+        except Exception as e:
+            print("ERROR: Error while sending", e)
+
+    def handle_server(self):
+        while not self.terminated:
+            try:
+                read_sockets, write_sockets, in_error = \
+                    select.select([self.socket, ], [self.socket, ], [], 5)
+            except select.error:
+                print("FATAL ERROR: Connection error")
+                self.socket.shutdown(2)
+                self.socket.close()
+            if len(read_sockets) > 0:
+                index = self.log.find('\n')
+                while index <= 0:
+                    self.log += self.socket.recv(2048).decode()
+                    index = self.log.find('\n')
+                data = self.log[:index]
+                self.log = self.log[index+1:]
+
+                if len(data) > 0:
+                        reply = self.data_received.emit(data)
+                if len(write_sockets) > 0 and reply:
+                    self.socket.send(reply.encode())
+        print("finished handling server")
 
     def on_receive(self, query):
-        print("receiving", query)
-        datapackages = json.loads(query)
+        pass
 
-        for button, datapackage in zip(self.buttonArray, datapackages):
-            if datapackages[0]["votes"] <= 0:
-                x=0
-            else:
-                x=datapackage["votes"]/datapackages[0]["votes"]
-            r=(1-x)*255
-            g=x*255
-            rgb = str(r)+","+str(g)+",0"
-            print(rgb)
-            button.setStyleSheet("QPushButton { text-align: left; padding: 10px; background-color: rgb("+rgb+");}")
-            self.wire_up_button(datapackage, button)
-
-    def wire_up_button(self, datapackage, button):
-        try:
-            button.clicked.disconnect()
-        except:
-            Exception
-
-        print("wireup")
-        title, songid = datapackage["title"], datapackage["songid"]
-        button.setText(title + " (" + str(datapackage["votes"]) + ")")
-        button.clicked.connect(lambda: self.upvote(songid))
-
-
-    def upvote(self, sid):
-        text = '{"action":"upvote", "value":"' + sid + '"}\n'
-        #if text!=self.lastvote:
-        self.lastvote=text
-        print(text)
-        self.send(text)
-
-
-
-
-class Window(QMainWindow):
-    def __init__(self):
-        super(Window, self).__init__()
-        self.setFixedSize()
-
+    def on_quit(self):
+        pass
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = VotingClient()
-    sys.exit(app.exec_())
+    client = SocketClient()
+    client.connect("10.42.24.206", 54345)
